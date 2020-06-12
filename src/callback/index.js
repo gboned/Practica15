519177c9a7b6324c6d1cad0cf76b0a679e488899
gboned <gboned@cifpfbmoll.eu>
2020-06-12 23:26:23 +0200

    Llamar dependencia y añadir URL API

--- src/callback/index.js
+++ src/callback/index.js
@@ -3,3 +3,8 @@
 /*      PROMESAS API      */
 /*                        */
 /**************************/
+
+// Llamo a la dependencia mediante módulo
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+// Añado la URL de la API
+const API_URL = "https://rickandmortyapi.com/api/character/";
